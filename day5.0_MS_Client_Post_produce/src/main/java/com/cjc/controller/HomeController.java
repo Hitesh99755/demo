4f35bc7fec45cab3c1d23df9fbd161ef207b6f94
hp997 <hp997@hitesh>
2024-05-16 16:30:43 +0530

    clean getmethod

--- day5.0_MS_Client_Post_produce/src/main/java/com/cjc/controller/HomeController.java
+++ day5.0_MS_Client_Post_produce/src/main/java/com/cjc/controller/HomeController.java
@@ -23,10 +23,7 @@ public class HomeController {
 	return "Insert Data Success full !!!   :)";
 	}
 
-	@GetMapping("/getData")
-	public String getData(@RequestBody Employee e){
-	hs.AllData(e);
-	return "Success";	
+
 
 		
 }
